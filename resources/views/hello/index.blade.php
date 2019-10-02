94d881c879dd1a747982d16dfc658fffa915ca53
Shunichiro Ida <shunichiroid@gmail.com>
2019-10-03 08:48:33 +0900

    Create form

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.index')
 
 @section('content')
-        <middleware>google.com</middleware>へのリンクです
-        <middleware>yahoo.co.jp</middleware>へのリンクです
+        <p>{{ $msg }}</p>
+        <table>
+        <form action="/hello" method="post">
+                {{ csrf_field() }}
+                <tr><th>name: </th><td><input type="text" name="name"></td></tr>
+                <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
+                <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+                <tr><th>submit: </th><td><input type="submit" name="submit"></td></tr>
+        </form>
+        </table>
 @endsection
