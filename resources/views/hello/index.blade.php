b5fc7b0ae5fbdcdb0ef813e900b2c53349f26948
Shunichiro Ida <shunichiroid@gmail.com>
2019-11-07 08:42:11 +0900

    Set cookies

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,8 +7,11 @@
 	@endif
 	<table>
 		<form action="/hello" method="post">
-			{{ csrf_field() }}
-			@if ($errors->has('name'))
+			@csrf
+			@if ($errors->has('msg'))
+			<tr><th>ERROR!</th><td>{{$errors->first('msg')}}</td></tr>
+			@endif
+			{{-- @if ($errors->has('name'))
 			<tr><th>ERROR!</th><td>{{$errors->first('name')}}</td></tr>
 			@endif
 			<tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
@@ -18,8 +21,11 @@
 			<tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
 			@if ($errors->has('age'))
 			<tr><th>ERROR!</th><td>{{$errors->first('age')}}</td></tr>
-			@endif
-			<tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
+			@endif --}}
+			{{-- <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr> --}}
+			<tr>
+				<th>Message: </th><td><input type="text" name="msg" value="{{old('msg')}}"></td>
+			</tr>
 			<tr><th>submit: </th><td><input type="submit" name="submit"></td></tr>
 		</form>
 	</table>
